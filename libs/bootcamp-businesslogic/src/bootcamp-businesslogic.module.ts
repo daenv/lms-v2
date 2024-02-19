851ffff3fa618aa86c813c2567b0d1ca9a815b3e
daenv <daenvle69@gmail.com>
2024-02-19 16:14:41 +0700

    Remove bootcamp-businesslogic module and service

--- libs/bootcamp-businesslogic/src/bootcamp-businesslogic.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BootcampBusinesslogicService } from './bootcamp-businesslogic.service';
-
-@Module({
-  providers: [BootcampBusinesslogicService],
-  exports: [BootcampBusinesslogicService],
-})
-export class BootcampBusinesslogicModule {}
--- libs/bootcamp-businesslogic/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './bootcamp-businesslogic.module';
-export * from './bootcamp-businesslogic.service';
