2cf92860ffb5e3219da78491e7a1f20d320e61ba
daenv <daenvle69@gmail.com>
2024-02-19 12:05:32 +0700

    Add global prefix and validation pipe to main.ts

--- apps/accounts/src/main.ts
+++ apps/accounts/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AccountsModule } from './accounts.module';
+import { useContainer } from 'class-validator';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AccountsModule);
-  await app.listen(3000);
+  useContainer(app, { fallbackOnErrors: true });
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const port = process.env.PORT || 3333;
+  await app.listen(port, () => {
+    console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  });
 }
 bootstrap();
