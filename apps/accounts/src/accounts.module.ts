a0d7c466cde21b1764b83c690f0d15a4fdf5313b
daenv <daenvle69@gmail.com>
2024-02-19 09:51:00 +0700

    Remove AccountsService provider from AccountsModule

--- apps/accounts/src/accounts.module.ts
+++ apps/accounts/src/accounts.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccountsController } from './accounts.controller';
-import { AccountsService } from './accounts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from '@app/common/configs';
 
 @Module({
   imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
   controllers: [AccountsController],
-  providers: [AccountsService],
+  // providers: [AccountsService],
 })
 export class AccountsModule {}
