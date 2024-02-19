cf80187a2e38dbf48c1c9c6b443a76b92a751fae
daenv <daenvle69@gmail.com>
2024-02-19 16:03:35 +0700

    Remove bootcamp-data module and service

--- libs/bootcamp-data/src/bootcamp-data.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BootcampDataService } from './bootcamp-data.service';
-
-@Module({
-  providers: [BootcampDataService],
-  exports: [BootcampDataService],
-})
-export class BootcampDataModule {}
--- libs/bootcamp-data/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './bootcamp-data.module';
-export * from './bootcamp-data.service';
