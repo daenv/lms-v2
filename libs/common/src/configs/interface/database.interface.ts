93f956187b1bf27e0db3faee235187f5c71b1b8c
daenv <daenvle69@gmail.com>
2024-02-22 12:28:00 +0700

    Add IDatabaseConfig interface for database configuration

--- libs/common/src/configs/interface/database.interface.ts
+++ libs/common/src/configs/interface/database.interface.ts
@@ -1,3 +1,5 @@
+/* This TypeScript code is defining an interface `IDatabaseConfig` that specifies the database
+configuration for different environments (development, production, and test). */
 import { IDatabaseConfigAttributes } from './database-attributes.interface';
 
 export interface IDatabaseConfig {
