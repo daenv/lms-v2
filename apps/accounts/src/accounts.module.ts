00a42f652220a6393888fa9bad58f9a0c3ead22d
daenv <daenvle69@gmail.com>
2024-02-22 16:37:35 +0700

    Refactor accounts module imports

--- apps/accounts/src/accounts.module.ts
+++ apps/accounts/src/accounts.module.ts
@@ -1,26 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AccountsController } from './accounts.controller';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { DatabaseModule } from '@app/common/databases/database.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
-        // entities: ['dist/**/*.entity{.ts,.js}'],
-        // synchronize: true,
-      }),
-    }),
-  ],
+  imports: [ConfigModule.forRoot(), DatabaseModule],
   controllers: [AccountsController],
-  // providers: [AccountsService],
+  providers: [ConfigService],
 })
 export class AccountsModule {}
