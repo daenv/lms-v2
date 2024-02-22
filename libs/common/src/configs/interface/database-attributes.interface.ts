e519d1d150cfd282b52eaad726222acade32c38f
daenv <daenvle69@gmail.com>
2024-02-22 12:27:57 +0700

    Refactor database configuration interface

--- libs/common/src/configs/interface/database-attributes.interface.ts
+++ libs/common/src/configs/interface/database-attributes.interface.ts
@@ -1,10 +1,15 @@
+/* This TypeScript code defines an interface named `IDatabaseConfigAttributes` that specifies the
+structure of database configuration attributes. Each attribute has a specific type such as `string`,
+`number`, or `boolean`. Here's a breakdown of the attributes: */
 export interface IDatabaseConfigAttributes {
-  username?: string;
-  password?: string;
-  host?: string;
-  database?: string;
-  port?: number | string;
-  dialect?: string;
-  logging?: boolean;
-  urlDatabase?: string;
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
+  dialect: string; // e.g., 'postgres', 'mysql'
+  synchronize?: boolean; // Optional, default: false (not recommended for production)
+  logging?: boolean; // Optional, default: false
+  entities?: string[]; // Optional, path to entities
+  migrations?: string[];
 }
