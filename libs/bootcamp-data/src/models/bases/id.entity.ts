9a5841881a4ebe2f37515212363a809b7a3a1f06
daenv <daenvle69@gmail.com>
2024-02-22 16:37:41 +0700

    Update IdEntity timestamps

--- libs/bootcamp-data/src/models/bases/id.entity.ts
+++ libs/bootcamp-data/src/models/bases/id.entity.ts
@@ -4,8 +4,8 @@ import { IIdEntity } from '../interfaces';
 export class IdEntity implements IIdEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
-  @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  @CreateDateColumn({ type: 'timestamptz' })
   createDateTime: Date;
-  @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  @CreateDateColumn({ type: 'timestamptz' })
   lastChangedDateTime: Date;
 }
