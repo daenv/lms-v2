b51afb23c343a07225a72327cd2e72fb44b42544
daenv <daenvle69@gmail.com>
2024-02-23 14:22:13 +0700

    Add cookie session and global pipes to main.ts

--- apps/accounts/src/main.ts
+++ apps/accounts/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AccountsModule } from './accounts.module';
+import * as cookieSession from 'cookie-session';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AccountsModule);
-  await app.listen(3000);
+  app.use(
+    cookieSession({
+      keys: ['asdasdasdsa'],
+    }),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  (app as any).set('etag', false);
+  app.use((req, res, next) => {
+    res.removeHeader('x-powered-by');
+    res.removeHeader('date');
+    next();
+  });
+  const port = process.env.PORT || 3333;
+  await app.listen(port, () => {
+    console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  });
 }
 bootstrap();
