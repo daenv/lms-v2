6c77046c7e8f2690806be201d10592808502c8a4
daenv <daenvle69@gmail.com>
2024-02-24 21:38:13 +0700

    Update imports in accounts module

--- apps/accounts/src/accounts.module.ts
+++ apps/accounts/src/accounts.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccountsController } from './accounts.controller';
 import { AccountsService } from './accounts.service';
-import { DatabaseModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@app/bootcamp-data';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AccountsController],
   providers: [AccountsService],
 })
