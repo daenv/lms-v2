ef5b91a62a3399840f83b176f667bb688e1ed00e
daenv <daenvle69@gmail.com>
2024-02-26 13:17:46 +0700

    Update DatabaseModule import in accounts.module.ts

--- apps/accounts/src/accounts.module.ts
+++ apps/accounts/src/accounts.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AccountsController } from './accounts.controller';
 import { AccountsService } from './accounts.service';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from '@app/bootcamp-data';
+import { DatabaseModule } from '@app/bootcamp-common';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
   controllers: [AccountsController],
   providers: [AccountsService],
 })
