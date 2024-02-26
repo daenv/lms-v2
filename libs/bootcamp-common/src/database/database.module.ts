dee34106e555f47de6477eaaffe854b85d2b5644
daenv <daenvle69@gmail.com>
2024-02-26 13:17:54 +0700

    Add database module for Postgres configuration

---
+++ libs/bootcamp-common/src/database/database.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_DATABASE: Joi.string().required(),
+        POSTGRES_USERNAME: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_SYNCHRONIZE: Joi.boolean().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow('POSTGRES_HOST'),
+        port: configService.getOrThrow('POSTGRES_PORT'),
+        database: configService.getOrThrow('POSTGRES_DATABASE'),
+        username: configService.getOrThrow('POSTGRES_USERNAME'),
+        password: configService.getOrThrow('POSTGRES_PASSWORD'),
+        autoLoadEntities: true,
+        synchronize: configService.getOrThrow('POSTGRES_SYNCHRONIZE'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
